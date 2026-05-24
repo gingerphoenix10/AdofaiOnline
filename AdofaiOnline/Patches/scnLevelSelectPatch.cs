38b000be6d7741ff029d797f76a97cf70df6a950
Phoenix <phoenix@gingerphoenix10.com>
2026-05-24 17:45:02 +0100

    Always spawn at the default spawn in scnLevelSelect to prevent desync when players join

---
+++ AdofaiOnline/Patches/scnLevelSelectPatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace AdofaiOnline.Patches;
+
+[HarmonyPatch(typeof(scnLevelSelect))]
+internal static class scnLevelSelectPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(scnLevelSelect.Start))]
+    internal static void StartPrefix()
+    {
+        if (!Networking.IsConnected)
+            return;
+        GCS.worldEntrance = null;
+    }
+}
